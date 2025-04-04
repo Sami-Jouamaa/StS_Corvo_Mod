0c4639939ca464890101146046551a583d717c8a
Sami-Jouamaa <samijouamaa@gmail.com>
2025-04-04 11:29:14 -0400

    added crossbow shot card, need to add the fear effect for melee attack cards

--- src/main/java/corvoattanomod/cards/attacks/CrossbowShot.java
+++ src/main/java/corvoattanomod/cards/attacks/CrossbowShot.java
@@ -10,8 +10,8 @@ import corvoattanomod.character.CorvoCharacter;
 import corvoattanomod.util.CardStats;
 import corvoattanomod.util.MeleeRanged;
 
-public class Strike extends BaseCard {
-    public static final String ID = makeID("Strike");
+public class CrossbowShot extends BaseCard {
+    public static  final String ID = makeID("CrossbowShot");
     private static final CardStats cardInfo = new CardStats(
             CorvoCharacter.Meta.CARD_COLOR,
             CardType.ATTACK,
@@ -19,26 +19,29 @@ public class Strike extends BaseCard {
             CardTarget.ENEMY,
             1
     );
-    private static final int damage = 6;
-    private static final int upg_damage = 9;
 
-    public Strike()
+    private static final int damage = 10;
+    private static final int upg_damage = 15;
+
+    public CrossbowShot()
     {
         super(ID, cardInfo);
 
         setDamage(damage, upg_damage);
         tags.add(CardTags.STARTER_STRIKE);
-        tags.add(CardTags.STRIKE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m)
     {
-        boolean triggersBonus = MeleeRanged.checkMelee();
+        boolean triggersBonus = MeleeRanged.checkRanged();
         if (triggersBonus)
         {
-            // Add fear and weaken for 3 turns
+            addToBot(new DamageAction(m, new DamageInfo(p, (int)Math.round(1.5*damage), DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
         }
-        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+        else
+        {
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        }
     }
 }
