f56422718a38ed5abfe8da5bb1ac6ea93faeba37
Sami-Jouamaa <samijouamaa@gmail.com>
2025-07-24 13:02:00 -0400

    shadow assassination done

---
+++ src/main/java/corvoattanomod/cards/attacks/ShadowAssassination.java
@@ -0,0 +1,56 @@
+package corvoattanomod.cards.attacks;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.vfx.combat.IntimidateEffect;
+import corvoattanomod.cards.BaseCard;
+import corvoattanomod.character.CorvoCharacter;
+import corvoattanomod.util.CardStats;
+import corvoattanomod.util.SpecialBonuses;
+
+public class ShadowAssassination extends BaseCard {
+    public static final String ID = makeID("ShadowAssassination");
+    private static final CardStats cardInfo = new CardStats(
+            CorvoCharacter.Meta.CARD_COLOR,
+            CardType.ATTACK,
+            CardRarity.UNCOMMON,
+            CardTarget.ENEMY,
+            2
+    );
+
+    private static final int DAMAGE = 15;
+    private static final int UPG_DAMAGE = 5;
+
+    public ShadowAssassination()
+    {
+        super(ID, cardInfo);
+        setDamage(DAMAGE, UPG_DAMAGE);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+        if(SpecialBonuses.checkMelee())
+        {
+            addToBot(new SFXAction("INTIMIDATE"));
+            addToBot(new VFXAction(p, new IntimidateEffect(AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY), 1.0F));
+            for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters)
+            {
+                addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)mo, (AbstractCreature)p, (AbstractPower)new WeakPower((AbstractCreature)mo, 3, false), 3, true, AbstractGameAction.AttackEffect.NONE));
+                addToBot((AbstractGameAction)new ApplyPowerAction((AbstractCreature)mo, (AbstractCreature)p, (AbstractPower)new VulnerablePower((AbstractCreature)mo, 3, false), 3, true, AbstractGameAction.AttackEffect.NONE));
+            }
+        }
+        CorvoCharacter.neutralStance = false;
+        CorvoCharacter.combatStance = false;
+        CorvoCharacter.stealthStance = true;
+    }
+}
